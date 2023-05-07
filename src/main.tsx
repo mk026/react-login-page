fee99fcdc8961dbbc430bf1a1257454d444230e0
Mikhail <mk0602604@gmail.com>
2023-05-07 23:43:22 +0300

    Create query-client and use query-client-provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
+import { queryClient } from "./api/queryClient";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
