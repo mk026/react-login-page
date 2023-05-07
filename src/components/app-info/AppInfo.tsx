a00a7fcb2cdfe101d4e17ed6b161ff5ea670d2a2
Mikhail <mk0602604@gmail.com>
2023-05-07 23:46:55 +0300

    Create app-info component

---
+++ src/components/app-info/AppInfo.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const AppInfo: FC = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum
+      dolores officiis fugit fuga optio excepturi vel, quo incidunt assumenda
+      dolore delectus ex? Pariatur mollitia possimus sint nihil aliquid
+      laudantium.
+    </div>
+  );
+};
+
+export default AppInfo;
