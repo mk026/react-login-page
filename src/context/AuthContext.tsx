66a849d08ca96be7b1ebc9e2a3769542923edcb3
Mikhail <mk0602604@gmail.com>
2023-05-08 23:51:07 +0300

    Add state to auth-context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,23 +1,36 @@
-import { FC, PropsWithChildren, createContext } from "react";
+import { FC, PropsWithChildren, createContext, useState } from "react";
 
 import { IUser } from "../models/user";
 
 export interface AuthContext {
   isAuth: boolean;
   user: IUser | null;
+  login: (user: IUser) => void;
+  logout: () => void;
 }
 
-export const AuthContext = createContext<AuthContext>({
-  isAuth: false,
-  user: null,
-});
-
-const initialValue: AuthContext = {
-  isAuth: false,
-  user: null,
-};
+export const AuthContext = createContext<AuthContext>({} as AuthContext);
 
 const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState<IUser | null>(null);
+
+  const login = (user: IUser) => {
+    setUser(user);
+    setIsAuth(true);
+  };
+  const logout = () => {
+    setUser(null);
+    setIsAuth(false);
+  };
+
+  const initialValue: AuthContext = {
+    isAuth,
+    user,
+    login,
+    logout,
+  };
+
   return (
     <AuthContext.Provider value={initialValue}>{children}</AuthContext.Provider>
   );
