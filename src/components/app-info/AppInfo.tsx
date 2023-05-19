7a80ec8e4fcc7463ac7357e02def5cf7ea1d72a9
Mikhail <mk0602604@gmail.com>
2023-05-19 23:04:07 +0300

    Add styles for app-info component

--- src/components/app-info/AppInfo.tsx
+++ src/components/app-info/AppInfo.tsx
@@ -1,10 +1,12 @@
 import { Card, Typography } from "@mui/material";
 import { FC } from "react";
 
+import classes from "./AppInfo.module.css";
+
 const AppInfo: FC = () => {
   return (
-    <Card>
-      <Typography>
+    <Card className={classes.info}>
+      <Typography className={classes.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum
         dolores officiis fugit fuga optio excepturi vel, quo incidunt assumenda
         dolore delectus ex? Pariatur mollitia possimus sint nihil aliquid
