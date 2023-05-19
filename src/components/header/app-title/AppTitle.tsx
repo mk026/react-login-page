eb8574fa33aabd7e2b5afe4a1165660c42245ce3
Mikhail <mk0602604@gmail.com>
2023-05-19 23:07:38 +0300

    Add styles for app-title component

--- src/components/header/app-title/AppTitle.tsx
+++ src/components/header/app-title/AppTitle.tsx
@@ -1,7 +1,14 @@
+import { Typography } from "@mui/material";
 import { FC } from "react";
 
+import classes from "./AppTitle.module.css";
+
 const AppTitle: FC = () => {
-  return <h1>React Login</h1>;
+  return (
+    <Typography variant="h1" className={classes.title}>
+      React Login
+    </Typography>
+  );
 };
 
 export default AppTitle;
