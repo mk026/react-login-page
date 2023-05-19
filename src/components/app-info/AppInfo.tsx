f03d29d5ba21078a810a0eb340f0e6575d7d78ab
Mikhail <mk0602604@gmail.com>
2023-05-19 23:02:31 +0300

    Refactor app-info component markup

--- src/components/app-info/AppInfo.tsx
+++ src/components/app-info/AppInfo.tsx
@@ -1,13 +1,16 @@
+import { Card, Typography } from "@mui/material";
 import { FC } from "react";
 
 const AppInfo: FC = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum
-      dolores officiis fugit fuga optio excepturi vel, quo incidunt assumenda
-      dolore delectus ex? Pariatur mollitia possimus sint nihil aliquid
-      laudantium.
-    </div>
+    <Card>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ipsum
+        dolores officiis fugit fuga optio excepturi vel, quo incidunt assumenda
+        dolore delectus ex? Pariatur mollitia possimus sint nihil aliquid
+        laudantium.
+      </Typography>
+    </Card>
   );
 };
 
