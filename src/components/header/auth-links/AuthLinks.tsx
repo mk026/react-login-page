76a4bb255f10e67579d92d46fa3ba0064282fed0
Mikhail <mk0602604@gmail.com>
2023-05-19 23:35:15 +0300

    Use router link component for auth-links

--- src/components/header/auth-links/AuthLinks.tsx
+++ src/components/header/auth-links/AuthLinks.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { Button, Stack } from "@mui/material";
+
 import { Paths } from "../../../routes";
 
 const AuthLinks: FC = () => {
   return (
-    <div>
-      <Link to={Paths.AUTH}>Login</Link>
-      <Link to={Paths.AUTH}>Register</Link>
-    </div>
+    <Stack direction="row">
+      <Button variant="outlined" component={Link} to={Paths.AUTH}>
+        Register
+      </Button>
+      <Button variant="contained" component={Link} to={Paths.AUTH}>
+        Login
+      </Button>
+    </Stack>
   );
 };
 
