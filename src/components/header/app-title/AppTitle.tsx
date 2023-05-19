1c0a4fa0013132b7dfd98983264349b66dab212a
Mikhail <mk0602604@gmail.com>
2023-05-19 23:38:22 +0300

    Use router link for app-title component

--- src/components/header/app-title/AppTitle.tsx
+++ src/components/header/app-title/AppTitle.tsx
@@ -1,11 +1,19 @@
-import { Typography } from "@mui/material";
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+
+import { Paths } from "../../../routes";
 
 import classes from "./AppTitle.module.css";
 
 const AppTitle: FC = () => {
   return (
-    <Typography variant="h1" className={classes.title}>
+    <Typography
+      variant="h1"
+      className={classes.title}
+      component={Link}
+      to={Paths.HOME}
+    >
       React Login
     </Typography>
   );
