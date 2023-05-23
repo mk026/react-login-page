017aab944e53d2ecb0900a0239985963bcb44ad5
Mikhail <mk0602604@gmail.com>
2023-05-24 02:27:00 +0300

    Refactor form-title component markup

--- src/components/shared/form-title/FormTitle.tsx
+++ src/components/shared/form-title/FormTitle.tsx
@@ -4,7 +4,11 @@ import { Typography } from "@mui/material";
 import classes from "./FormTitle.module.css";
 
 const FormTitle: FC<PropsWithChildren> = ({ children }) => {
-  return <Typography className={classes.title}>{children}</Typography>;
+  return (
+    <Typography variant="h4" className={classes.title}>
+      {children}
+    </Typography>
+  );
 };
 
 export default FormTitle;
