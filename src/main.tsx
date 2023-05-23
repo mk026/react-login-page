bb6f01adb6ef52f62694f1a760019441987192b7
Mikhail <mk0602604@gmail.com>
2023-05-24 02:25:40 +0300

    Refactor imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { StyledEngineProvider } from "@mui/material";
 
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
 import { queryClient } from "./api/queryClient";
 
 import "./styles/global.css";
-import { StyledEngineProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
