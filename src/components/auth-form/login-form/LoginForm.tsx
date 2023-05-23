fc2735a20bd0bf94a04172180a38f37d1a28c7a7
Mikhail <mk0602604@gmail.com>
2023-05-23 23:43:36 +0300

    Refactor login-form markup

--- src/components/auth-form/login-form/LoginForm.tsx
+++ src/components/auth-form/login-form/LoginForm.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 
 import { useLoginForm } from "../../../hooks/useLoginForm";
 import Form from "../../shared/form";
+import FormCard from "../../shared/form-card";
+import FormTitle from "../../shared/form-title";
+import FormInfo from "../../shared/form-info";
 
 interface LoginFormProps {
   changeAuthMode: () => void;
@@ -12,18 +15,23 @@ const LoginForm: FC<LoginFormProps> = ({ changeAuthMode }) => {
   const { methods, onSubmit, isLoading } = useLoginForm();
 
   return (
-    <Box>
-      <Typography>Login</Typography>
+    <FormCard>
+      <FormTitle>Login</FormTitle>
       <Form methods={methods} onSubmit={onSubmit}>
-        <Form.Field label="Email" name="email" />
-        <Form.Field label="Password" name="password" type="password" />
+        <Form.Field fullWidth label="Email" name="email" />
+        <Form.Field
+          fullWidth
+          label="Password"
+          name="password"
+          type="password"
+        />
         <Form.SubmitButton isLoading={isLoading}>Login</Form.SubmitButton>
       </Form>
-      <Typography>
+      <FormInfo>
         Don't have an account?{" "}
         <Button onClick={changeAuthMode}>Register</Button>
-      </Typography>
-    </Box>
+      </FormInfo>
+    </FormCard>
   );
 };
 
