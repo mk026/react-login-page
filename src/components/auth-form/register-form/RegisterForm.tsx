335f45d77e19d6219ab8d73095989e2717a56b63
Mikhail <mk0602604@gmail.com>
2023-05-23 23:44:16 +0300

    Refactor register-form markup

--- src/components/auth-form/register-form/RegisterForm.tsx
+++ src/components/auth-form/register-form/RegisterForm.tsx
@@ -1,8 +1,11 @@
 import { FC } from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 
 import { useRegisterForm } from "../../../hooks/useRegisterForm";
 import Form from "../../shared/form";
+import FormCard from "../../shared/form-card";
+import FormTitle from "../../shared/form-title";
+import FormInfo from "../../shared/form-info";
 
 interface RegisterFormProps {
   changeAuthMode: () => void;
@@ -12,24 +15,30 @@ const RegisterForm: FC<RegisterFormProps> = ({ changeAuthMode }) => {
   const { methods, onSubmit, isLoading } = useRegisterForm();
 
   return (
-    <Box>
-      <Typography>Register</Typography>
+    <FormCard>
+      <FormTitle>Register</FormTitle>
       <Form methods={methods} onSubmit={onSubmit}>
-        <Form.Field label="Name" name="name" />
-        <Form.Field label="Bio" name="bio" />
-        <Form.Field label="Email" name="email" />
-        <Form.Field label="Password" name="password" type="password" />
+        <Form.Field fullWidth label="Name" name="name" />
+        <Form.Field fullWidth multiline label="Bio" name="bio" />
+        <Form.Field fullWidth label="Email" name="email" />
         <Form.Field
+          fullWidth
+          label="Password"
+          name="password"
+          type="password"
+        />
+        <Form.Field
+          fullWidth
           label="Confirm Password"
           name="confirmPassword"
           type="password"
         />
         <Form.SubmitButton isLoading={isLoading}>Register</Form.SubmitButton>
       </Form>
-      <Typography>
+      <FormInfo>
         Already have an account? <Button onClick={changeAuthMode}>Login</Button>
-      </Typography>
-    </Box>
+      </FormInfo>
+    </FormCard>
   );
 };
 
